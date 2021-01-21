c4179705aa45c74a7254caeafcf39df3ea737a3e
removio <76274060+removio@users.noreply.github.com>
2021-01-21 15:38:39 +0200

    1

---
+++ js/translater.js
@@ -0,0 +1,230 @@
+var new_lang = {
+    "lt0": "XX",
+
+    "lt1": "¡Deshazte de las verrugas y los papilomas de forma rápida y segura!",
+    
+    "lt2": "OFERTA ESPECIAL",
+    
+    "lt3": "hasta el final de la promoción quedan<div class=\"form-header__timer-script\"><span class=\"hoursSH\">11</span>:<span class=\"minutesSH\">11</span>:<span class=\"secondsSH\">11</span></div>",
+    
+    "lt4": "Precio habitual",
+    
+    "lt5": "Precio de promoción",
+    
+    "lt6": "Ordenar",
+    
+    "lt7": "ACCIÓN",
+    
+    "lt8": "Eficaz contra<br> las diferentes cepas <br>del virus del papiloma",
+    
+    "lt9": "horas",
+    
+    "lt10": "Destruye completamente<br> el patógeno durante<br> las primeras 48 horas.",
+    
+    "lt11": "En conjunto",
+    
+    "lt12": "Bloquea la actividad del virus<br> y activa la protección<br> inmunológica",
+    
+    "lt13": "¡ELIMINA LOS PAPILOMAS Y PREVIENE EL CÁNCER DE LA PIEL! <br><span>XX</span>- ¡SALVA VIDAS!",
+    
+    "lt14": "La combinación de los componentes activos elimina las formaciones de la piel y previene su degeneración en tumores cancerosos.",
+    
+    "lt15": "Papilomas<br> en el cuello",
+    
+    "lt16": "Papilomas<br> en la<br> axila",
+    
+    "lt17": "Papilomas en las piernas<br> que han comenzado<br> a degenerar.",
+    
+    "lt18": "Papiloma,<br> que se ha convertido en cáncer.",
+    
+    "lt19": "Causas de los papilomas",
+    
+    "lt20": "Los papilomas son excrecencias de la piel causadas por una infección viral, llamada: Virus de papiloma humano o VPH. Se transmite exclusivamente de persona a persona a través de daños en la piel. El período de incubación de esta enfermedad oscila entre unos pocos días y hasta décadas de años. Tan pronto como el cuerpo se debilita debido a enfermedad, estrés, embarazo, los papilomas aparecen en la dermis",
+    
+    "lt21": "<span>¡XX</span> combate todo tipo de papilomas!",
+    
+    "lt22": "<span style=\"color: #0158ff;\">Los papilomas simples (vulgares)</span> generalmente toman forma en las extremidades superiores.",
+    
+    "lt23": "<span style=\"color: #127abe;\">Los papilomas planos</span> se diseminan en el cuello, cara,<br> extremidades superiores.",
+    
+    "lt24": "<span style=\"color: #0ba1e3;\">Los papilomas plantares</span> afectan los pies.",
+    
+    "lt25": "<span style=\"color: #1dc1dd;\">Los papilomas filiformes</span> su lugar de localización es el cuello, la cara, la ingle y<br> las zonas axilares",
+    
+    "lt26": "<span style=\"color: #e2000f;\">Los condilomas</span> también conocidos como papilomas puntiagudos<br> son especies particularmente peligrosos",
+    
+    "lt27": "¡DESHAZTE DE ELLOS PRONTO!",
+    
+    "lt28": "¡El Gel <span>XX</span> es una forma simple y efectiva de deshacerse de los papilomas y las verrugas!",
+    
+    "lt29": "Suprime la actividad de las células virales al penetrar en las capas más profundas de la dermis",
+    
+    "lt30": "Remueve sin dolor las neoplasias, previniendo el desarrollo de los papilomas",
+    
+    "lt31": "Restaura las áreas afectadas de la piel",
+    
+    "lt32": "Aumenta la resistencia contra los virus, las infecciones por hongos, previene el desarrollo de enfermedades oncológicas",
+    
+    "lt33": "OFERTA ESPECIAL",
+    
+    "lt34": "hasta el final de la promoción quedan<div class=\"form-header__timer-script\"><span class=\"hoursSH\">11</span>:<span class=\"minutesSH\">11</span>:<span class=\"secondsSH\">11</span></div>",
+    
+    "lt35": "Precio habitual",
+    
+    "lt36": "Precio de promoción",
+    
+    "lt37": "Ordenar",
+    
+    "lt38": "ORDENAR",
+    
+    "lt39": "¡El Gel <span>XX</span> es una forma simple y efectiva de deshacerse de los papilomas y las verrugas!",
+    
+    "lt40": "Aceite de jengibre",
+    
+    "lt41": "Posee poderosas propiedades antibacterianas y antiinflamatorias que ayudan a prevenir la propagación del virus",
+    
+    "lt42": "Extracto de consuelda",
+    
+    "lt43": "Se utiliza en medicina para tratar<br> enfermedades inflamatorias Descompone<br> las verrugas y los papilomas de forma rápida e indolora",
+    
+    "lt44": "Extracto de bálsamo de limón o melisa",
+    
+    "lt45": "Posee un efecto antiviral pronunciado. Mejora la producción de colágeno y elastina, consistentes en proteínas esenciales para una piel sana",
+    
+    "lt46": "Tocoferol",
+    
+    "lt47": "Tiene propiedades antiinflamatorias y antisépticas, ayuda a restaurar la epidermis, nutre la piel con vitaminas y microelementos",
+    
+    "lt48": "Aceite de Cymbopogon citratus (limoncillo)",
+    
+    "lt49": "Gracias a su rica composición química, es eficaz en el tratamiento de cicatrices, manchas e imperfecciones cutáneas. Elimina la inflamación, mejora la inmunidad local",
+    
+    "lt50": "Aceite de árbol de té",
+    
+    "lt51": "Impide la nutrición de los papilomas y las verrugas, asegurando el perecimiento de las neoplasias cutáneas y previniendo la aparición de otras nuevas",
+    
+    "lt52": "Canela",
+    
+    "lt53": "Ejerce un fuerte efecto antiviral, previene la aparición del cáncer, bloquea la división patógena de las células.",
+    
+    "lt54": "Niacinamida",
+    
+    "lt55": "Aumenta la inmunidad local. Ocasiona un efecto cicatrizante y bactericida, proporcionando una curación rápida e indolora.",
+    
+    "lt56": "<span>¡XX</span> es tu nueva vida sin papilomas y verrugas!",
+    
+    "lt57": "Impacto puntual",
+    
+    "lt58": "Destruye los papilomas desde su raíz",
+    
+    "lt59": "Elimina el virus",
+    
+    "lt60": "Sin dejar rastros y sin dolor",
+    
+    "lt61": "Acción delicada",
+    
+    "lt62": "No daña la piel,<br> restaura el tejido",
+    
+    "lt63": "ORDENAR",
+    
+    "lt64": "Ensayos clínicos",
+    
+    "lt65": "En el año 2019, los especialistas del Instituto de Dermatovenerología realizaron ensayos clínicos del producto <span>XX</span> en un grupo de 658 voluntarios que padecían de VPH y experimentaban papilomas y verrugas en el cuerpo. Fueron obtenidos los siguientes datos:",
+    
+    "lt66": "Liberación de formaciones externas (papilomas, verrugas genitales, verrugas comúnes)",
+    
+    "lt67": "Eliminación del virus causante de la reincidencia (resultados observados después de 3 meses)",
+    
+    "lt68": "El 99% de los voluntarios carecían de efectos secundarios",
+    
+    "lt69": "Opinión de un especialista",
+    
+    "lt70": "Casi todas las personas tienen papilomas y verrugas. Estas son formaciones benignas en la epidermis. Pero su perfidia consiste en que, tarde o temprano, pueden volverse malignos y conducir a enfermedades oncológicas. Por lo tanto, los papilomas deben eliminarse, incluso si no causan molestias desde el punto de vista cosmético.<br> A mis pacientes les recomiendo usar el gel <span>XX</span>.",
+    
+    "lt71": "Ignacio Márques.<br> Médico dermatólogo",
+    
+    "lt72": "Comentarios de los clientes",
+    
+    "lt73": "Eduardo, 50 años",
+    
+    "lt74": "Tuve papilomas desde la niñez, pero nunca les di mucha importancia. En realidad, pensaba que era una especie de lunares y que no era necesario luchar con esto en absoluto. Mi esposa me abrió los ojos al hecho de que estos no son de ninguna manera lunares, y que pueden conducir a una transformación en cáncer. Ante su insistencia, fui al médico que me prescribió el gel <span>XX</span>. Los papilomas desaparecieron rápidamente y sin dolor, sin dejar rastro alguno",
+    
+    "lt75": "Verónica, 35 años",
+    
+    "lt76": "Mi novio me infectó de verrugas genitales. Fuimos juntos a un médico para el tratamiento, quien nos dijo que nuestro virus era altamente cancerígeno, es decir, que era muy probable que llevase a cáncer. ¡Realmente nos asustamos no de broma! Nos recetaron un montón de pastillas, pero el efecto fue insignificante. Me enteré de <span>XX</span> por una amiga, cuando le conté mi problema y decidí intentarlo. Durante un mes nos aplicamos el gel en las áreas problemáticas, todo pasó bastante rápido. ¡Dos meses después dimos de nuevo análisis, los resultados no mostraron nada!",
+    
+    "lt77": "Ana, 43 años",
+    
+    "lt78": "Me surgieron verrugas hace unos cinco años. De qué manera no intenté luchar contra esto: probé varios ungüentos, cautericé, y hasta me los quité quirúrgicamente. Nada me ayudó. La solución la encontré accidentalmente en internet. Vi un artículo sobre <span>XX</span>. Hice pedido y lo probé. Dos semanas después, noté que las verrugas comenzaron a volverse negras y luego se cayeron totalmente. Apenas un mes después del primer uso, mi piel estaba ya completamente limpia y no quedó ni una sola cicatriz.",
+    
+    "lt79": "Eduardo, 50 años",
+    
+    "lt80": "Tuve papilomas desde la niñez, pero nunca les di mucha importancia. En realidad, pensaba que era una especie de lunares y que no era necesario luchar con esto en absoluto. Mi esposa me abrió los ojos al hecho de que estos no son de ninguna manera lunares, y que pueden conducir a una Este es el Este es el remedio más eficaz y seguro disponible en este momento. XX combate los papilomas y las verrugas en las capas más profundas de la piel, eliminando por completo las neoplasias no deseadas .",
+    
+    "lt81": "Verónica, 35 años",
+    
+    "lt82": "Mi novio me infectó de verrugas genitales. Fuimos juntos a un médico para el tratamiento, quien nos dijo que nuestro virus era altamente cancerígeno, es decir, que era muy probable que llevase a cáncer. ¡Realmente nos asustamos no de broma! Nos recetaron un montón de pastillas, pero el efecto fue insignificante. Me enteré de <span>XX</span> por una amiga, cuando le conté mi problema y decidí intentarlo. Durante un mes nos aplicamos el gel en las áreas problemáticas, todo pasó bastante rápido. ¡Dos meses después dimos de nuevo análisis, los resultados no mostraron nada!",
+    
+    "lt83": "Ana, 43 años",
+    
+    "lt84": "Me surgieron verrugas hace unos cinco años. De qué manera no intenté luchar contra esto: probé varios ungüentos, cautericé, y hasta me los quité quirúrgicamente. Nada me ayudó. La solución la encontré accidentalmente en internet. Vi un artículo sobre <span>XX</span>. Hice pedido y lo probé. Dos semanas después, noté que las verrugas comenzaron a volverse negras y luego se cayeron totalmente. Apenas un mes después del primer uso, mi piel estaba ya completamente limpia y no quedó ni una sola cicatriz.",
+    
+    "lt85": "Eduardo, 50 años",
+    
+    "lt86": "Tuve papilomas desde la niñez, pero nunca les di mucha importancia. En realidad, pensaba que era una especie de lunares y que no era necesario luchar con esto en absoluto. Mi esposa me abrió los ojos al hecho de que estos no son de ninguna manera lunares, y que pueden conducir a una transformación en cáncer. Ante su insistencia, fui al médico que me prescribió el gel <span>XX</span>. Los papilomas desaparecieron rápidamente y sin dolor, sin dejar rastro alguno.",
+    
+    "lt87": "XX",
+    
+    "lt88": "¡Deshazte de las verrugas y las papilas de forma rápida y segura!",
+    
+    "lt89": "OFERTA ESPECIAL",
+    
+    "lt90": "hasta el final de la promoción quedan<div class=\"form-header__timer-script\"><span class=\"hoursSH\">11</span>:<span class=\"minutesSH\">11</span>:<span class=\"secondsSH\">11</span></div>",
+    
+    "lt91": "Precio habitual",
+    
+    "lt92": "Precio de promoción",
+    
+    "lt93": "Ordenar",
+    
+    "lt94": "ACCIÓN",
+    
+    "lt95": "Eficaz contra<br> las diferentes cepas <br>del virus del papiloma",
+    
+    "lt96": "horas",
+    
+    "lt97": "Destruye completamente<br> el patógeno durante<br> las primeras 48 horas.",
+    
+    "lt98": "En conjunto",
+    
+    "lt99": "Bloquea la actividad del virus<br> y activa la protección<br> inmunológica",
+    
+    "lt100": "ORDENAR en 3 pasos",
+    
+    "lt101": "PASO 1",
+    
+    "lt102": "Deja una solicitud<br> en nuestro sitio web",
+    
+    "lt103": "PASO 2",
+    
+    "lt104": "Nuestro operador se pondrá en contacto <br>contigo para<br> confirmar el pedido.",
+    
+    "lt105": "PASO 3",
+    
+    "lt106": "Pago en línea o<br> al recibir<br> el pedido"
+        
+    
+
+
+
+};
+function Translater () {
+ for (class_name in new_lang) {
+ var elements = document.getElementsByClassName(class_name);
+ if (elements.length) {
+ for (key in elements) {
+ elements[key].innerHTML = new_lang[class_name];
+ }
+ }
+ }
+ };
